4a6d97a6ebc21db46c1ec48f390c67953c466245
showc7 <showc7@gmail.com>
2016-11-13 20:57:55 +0300

    initial state

---
+++ SMTP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NLog;
+
+namespace SMTP
+{
+    class Program
+    {
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 25);
+            _logger.Info("Server started on " + endPoint.Address + ":" + endPoint.Port);
+            TcpListener listener = new TcpListener(endPoint);
+            listener.Start();
+
+            while(true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                SMTPServer handler = new SMTPServer();
+                //servers.Add(handler);
+                handler.Init(client);
+                Thread thread = new Thread(new ThreadStart(handler.Run));
+                thread.Start();
+            }
+        }
+    }
+}
---
+++ SMTP/SMTPServer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using NLog;
+
+namespace SMTP
+{
+    internal class SMTPServer
+    {
+        private Logger _logger = LogManager.GetCurrentClassLogger();
+
+        private TcpClient client;
+        public SMTPServer()
+        {
+        }
+
+        public void Init(TcpClient client)
+        {
+            this.client = client;
+        }
+
+        public void Run()
+        {
+            _logger.Info("client connected " + client.Client.RemoteEndPoint);
+
+            while(true)
+            {
+                if(!AcceptMessage())
+                {
+                    break;
+                }
+            }
+        }
+
+        private bool AcceptMessage()
+        {
+            string message = string.Empty;
+            try
+            {
+                message = Read();
+                _logger.Info("client message: {" + message + "}");
+            }
+            catch(Exception e)
+            {
+                _logger.Error(e.Message + "\n" + e.Source + "\n" + e.StackTrace);
+                return false;
+            }
+
+            if(message.Length > 0)
+            {
+                if(message.StartsWith("QUIT"))
+                {
+                    client.Close();
+                    return false;
+                }
+                if(message.StartsWith("EHLO"))
+                {
+                    Write("250 OK");
+                }
+                if(message.StartsWith("RCPT TO"))
+                {
+                    Write("250 OK");
+                }
+                if(message.StartsWith("MAIL FROM"))
+                {
+                    Write("250 OK");
+                }
+                if(message.StartsWith("DATA"))
+                {
+                    Write("354 Start mail input; end with");
+                    message = Read();
+                    Write("250 OK");
+                }
+            }
+
+            return true;
+        }
+
+        private string Read()
+        {
+            int bufferSize = 8192;
+            byte[] messageBytes = new byte[bufferSize];
+            NetworkStream clientStream = client.GetStream();
+            ASCIIEncoding encoder = new ASCIIEncoding();
+            int bytesRead = clientStream.Read(messageBytes, 0, bufferSize);
+            return encoder.GetString(messageBytes,0,bytesRead);
+        }
+
+        private void Write(string message)
+        {
+            NetworkStream clientStream = client.GetStream();
+            ASCIIEncoding encoder = new ASCIIEncoding();
+            byte[] buffer = encoder.GetBytes(message + "\r\n");
+            clientStream.Write(buffer,0,buffer.Length);
+            clientStream.Flush();
+        }
+    }
+}
